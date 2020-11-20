9abf3ebdc80447a2f852ae8742f190ab50a3413e
Shaquan Robinson <56005808+Shaq44@users.noreply.github.com>
2020-11-20 13:21:14 -0800

    Create BarkingDog.java

---
+++ BarkingDog.java
@@ -0,0 +1,17 @@
+ public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+
+        if (barking && hourOfDay < 0){
+            System.out.println("False");
+
+        } else if (barking && hourOfDay > 23) {
+            System.out.println("False");
+        } else if ( barking && hourOfDay >= 0){
+            System.out.println("true");
+        } else if (barking && hourOfDay >= 23){
+            System.out.println("True");
+
+        }
+        return false;
+
+
+    }
